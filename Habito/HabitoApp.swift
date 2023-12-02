ca20343d3cea5961acac67ba523f2b89dc36a11d
MaksGorobets <gorobets542@gmail.com>
2023-12-02 18:46:11 +0200

    Initial Commit

---
+++ Habito/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by Maks Winters on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
